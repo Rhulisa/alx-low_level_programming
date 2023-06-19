467b535a30ef848f099c52b8f450b764f1c1229d
Rhulisa <rhulisam@gmail.com>
2023-06-19 05:13:28 -0700

    cprogrming

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+#include<stdio.h>
+/**
+ * main function
+ */
+int main(void)
+{
+	Printf("Size of a char: %d byte(s)\n",sizeof(char));
+	Printf("Size of an int: %d byte(s)\n",sizeof(int));
+	Printf("Size of a long int: %d byte(s)\n",sizeof(long int));
+	Printf("Size of a long long int: %d byte(s)\n",sizeof(long long int));
+	Printf("Size of a float: %d byte(s)\n",sizeof(float));
+	return(0);
+}
