854f4f58052b0e77ab3cef5bbb673cc27550f5db
Rhulisa <rhulisam@gmail.com>
2023-06-23 07:43:45 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * *maul - multiply integers
+ * *Return: Always return 0 (success)
+ */
+
+int mul(int a, int b)
+{
+	int result = a * b;
+
+	return (result);
+}
