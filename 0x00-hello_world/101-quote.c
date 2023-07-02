2001083a60f885987766bc7a87924c441187754e
Rhulisa <rhulisam@gmail.com>
2023-07-02 10:45:33 -0700

    intel

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<unistd.h>
+/**
+ * *main - Entry point
+ * *Return: Always return 1 (success)
+ * */
+
+int main(void)
+{
+		fput("and that piece of art is useful\" - Dora korpar, 2015-10-19\r", stdout);
+		return(1);
+}
