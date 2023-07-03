6ef44ac4bcce386f66d9e097c93f87dedab54ce5
Rhulisa <rhulisam@gmail.com>
2023-07-03 10:37:38 -0700

    ./10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * *pring_last_digit - print last digit
+ * *
+ * * @n: character to be checked
+ * *
+ * *Return: 1
+ * */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
