dd88434158a5e0d78566e09da3ca42991d0926f6
Rhulisa <rhulisam@gmail.com>
2023-07-03 10:11:11 -0700

    ./5-sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+
+#include "main.h"
+/**
+ * *pring_sign - print ssign numbers
+ * *
+ * * @n: character to be checked
+ * *
+ * *Return: 1
+ * */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else 
+	{
+		_putchar(48);
+		return (0);
+	}
+}
+
