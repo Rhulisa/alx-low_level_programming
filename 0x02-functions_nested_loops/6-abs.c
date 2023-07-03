940686e0851e0ecf089330c23df650ce619f2a52
Rhulisa <rhulisam@gmail.com>
2023-07-03 10:16:43 -0700

    ./6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+
+#include "main.h"
+/**
+ * *pring_sign - print ssign numbers
+ * *
+ * * @n: character to be checked
+ * *
+ * *Return: 1
+ * */
+
+int _abs(int c)
+{
+	if (c < 0)
+	{
+		int abs;
+
+		abs = c * -1;
+		return (abs);
+	}
+	return (c);
+}
