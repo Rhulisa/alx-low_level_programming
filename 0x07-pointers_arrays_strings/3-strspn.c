d3631f0befa604b4bce5d817f43a7164817dde1a
Rhulisa <rhulisam@gmail.com>
2023-07-04 12:31:32 -0700

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+
+#include "main.h"
+/**
+ * *main - Entry point
+ * *
+ * *Return: Always return 0 (success)
+ * */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int bytes = 0;
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+			{
+				bytes++;
+				break;
+			}
+			else if (accept[i + 1] == '\0')
+				return (bytes);
+		}
+		s++;
+	}
+	return (bytes);
+}
+
